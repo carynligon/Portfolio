413c1e83daaad5c347a5c7666eb7457b5f718160
Caryn Ligon <carynligon@gmail.com>
2016-09-05 11:39:09 -0500

    styling contact form

--- app/scripts/components/Contact.js
+++ app/scripts/components/Contact.js
@@ -7,14 +7,13 @@ export default React.createClass({
         <form className="contact-form" action="mailto:hello@carynligon.com" method="post" encType="text/plain">
           <h2>Let's talk!</h2>
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" placeholder="Name"/>
+          <input type="text" name="name" id="name"/>
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" placeholder="Email"/>
+          <input type="email" name="email" id="email"/>
           <label htmlFor="message">Message</label>
-          <textarea name="message" id="message" placeholder="Hi nice to meet you!"/>
+          <textarea name="message" id="message"/>
 
           <input type="submit" value="Send"/>
-          <input type="reset" value="Reset"/>
         </form>
       </section>
     );
