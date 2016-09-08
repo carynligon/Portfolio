6b90b7fa8d233cf283495a8ad0f497b8ffe7a258
Caryn Ligon <carynligon@gmail.com>
2016-09-07 20:49:41 -0500

    fixed header style and added about me text

--- app/scripts/components/About.js
+++ app/scripts/components/About.js
@@ -10,8 +10,8 @@ export default React.createClass({
         <div id="image" alt="image of Caryn">
         </div>
         <div id="about-paragraph-wrapper">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>I am a junior front-end developer with experience building responsive, single page web applications with HTML, CSS, JavaScript, React, Backbone, AJAX, API's and JQuery. I am excited to enter the industry and to join a team where I can contribute and grow my skills. I value hard work, integrity, loyalty, and relationships. I love to learn and I thrive most when challenged.</p>
+          <p>Other things I love besides development: my husband, my dog, Tina Fey, cooking, coffee, rock climbing, running, mixology. I tell bad jokes and love my family and friends more than everything because they laugh.</p>
         </div>
       </main>
     );
--- app/scripts/components/Nav.js
+++ app/scripts/components/Nav.js
@@ -8,6 +8,7 @@ export default React.createClass({
         <Link to="/">Home</Link>
         <Link to="/projects">Projects</Link>
         <a href="/#contact-section">Contact</a>
+        <a href="#">Resume</a>
       </nav>
     );
   }
